216611554274c5059ddee41a01260001f6ccbcc8
samaysar <samaysar@gmail.com>
2016-05-07 15:21:05 +0200

    Exception Class modif
    Cleaned exception class ctor and added reason to error message.

--- Waterfall/Base/WaterfallException.cs
+++ Waterfall/Base/WaterfallException.cs
@@ -11,64 +11,79 @@ namespace Waterfall.Base
         /// Unknown Error
         /// </summary>
         Unknown = 0,
+
         /// <summary>
         /// WaterfallMapAttribute is missing
         /// </summary>
         MissingWaterfallMapAttributeAttribute,
+
         /// <summary>
         /// Type is missing in WaterfallMapAttribute/WaterfallWorkAttribute
         /// </summary>
         WaterfallAttributeSuppliedTypeIsNull,
+
         /// <summary>
         /// Type provided in WaterfallMapAttribute/WaterfallWorkAttribute is not correctly derived
         /// </summary>
         WaterfallWorkIsNotDerivedCorrectly,
+
         /// <summary>
         /// Map field is not int type
         /// </summary>
         MapMemberIsNotIntType,
+
         /// <summary>
         /// Map is neither class type (for int based waterfall) nor enum type (for enum based waterfall)
         /// </summary>
         UnknownTypeOfWaterfallMap,
+
         /// <summary>
         /// Map has known issue
         /// </summary>
         InvalidWaterfallMap,
+
         /// <summary>
         /// Waterfall work does not supply default parameter less ctor
         /// </summary>
         WaterfallWorkDoesNotDefineDefaultParameterLessCtor,
+
         /// <summary>
         /// More than 1 waterfall has same value associated to it (same enum/int)
         /// </summary>
         TwoOrMoreWaterfallWorksReturnsSameUniqueIdentifier,
+
         /// <summary>
         /// Waterservice either disposed or canceled
         /// </summary>
         WaterfallServiceDisposedOrCancellationDemanded,
+
         /// <summary>
         /// Enum based map is not inherited from int
         /// </summary>
         EnumNotInheritedFromInt,
+
         /// <summary>
         /// Enum map doesn't contain a member with associated int value as -1
         /// </summary>
         EnumDoesNotDefineEndOfWaterfallAsMinusOne,
+
         /// <summary>
         /// Enum values are not continuously increasing like -1,0,1,2,3...
         /// </summary>
         EnumValuesAreNotContinuouslyIncreasing,
+
         /// <summary>
         /// Thrown only when WATERFALL_DEBUG (conditional compilation symbol) is defined
         /// and when possible looped call is detected during waterfall execution (based on the policy that each waterfall work
         /// can execute only once for a given pair of input/result).
         /// </summary>
         WaterfallExceededMaxIterations,
+
         /// <summary>
         /// When a given waterfall work returns a value (enum/int) for which work is undefined by the map
         /// </summary>
         WaterfallNotDefined,
+
         /// <summary>
         /// When redundancy is identified during construction time
         /// </summary>
@@ -80,9 +95,8 @@ namespace Waterfall.Base
     /// </summary>
     public sealed class WaterfallException : Exception
     {
-        private WaterfallException(string message, Exception inner) : base(message, inner) {}
-
-        internal WaterfallException(WaterfallErrorType errorType, string message, Exception inner = null) : this(message, inner)
+        internal WaterfallException(WaterfallErrorType errorType, string message, Exception inner = null)
+            : base($"Reason:{errorType}. {message}", inner)
         {
             ErrorType = errorType;
         }
